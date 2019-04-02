953a0295000ed586c14cd06b48092496b6be15ee
user <user@Yasir>
2019-04-02 13:13:31 +0530

    new calculator

--- calculator/src/calculator/calculator.java
+++ calculator/src/calculator/calculator.java
@@ -3,24 +3,34 @@ import java.util.*;
 public class calculator {
 		public static void main(String args[]) {
 			Scanner sc=new Scanner(System.in);
-			int a,b;char choice;
+			int a,b,choice;
 			a=sc.nextInt();
 			b=sc.nextInt();
-			System.out.println("Enter + for add or - for subtract or * for multiple or / for divide");
-			choice=sc.next().charAt(0);
-			if(choice=='+') {
-				System.out.println(a+b);
-			}
-			if(choice=='-') {
-				System.out.println(a-b);
-			}
-			if(choice=='*') {
-				System.out.println(a*b);
-				
-			}
-			if(choice=='/')
-				System.out.println(a/b);
+			System.out.println("Enter 1 for add or 2 for subtract or 3 for multiple or 4 for divide");
+			choice=sc.nextInt();
+			calculator ob=new calculator();
+			if(choice==1)
+			System.out.println(ob.add(a,b));
+			if(choice==2)
+			System.out.println(ob.sub(a,b));
+			if(choice == 3)
+			System.out.println(ob.mul(a,b));
+			if(choice ==4)
+			System.out.println(ob.div(a,b));
 			sc.close();
+		}
+		
+		public int add(int a,int b) {
+			return (a+b);
+		}
+		public int sub(int a,int b) {
+		 return (a-b);
+		}
+		public int mul(int a,int b) {
+			return (a*b);
+		}
+		public int div(int a,int b) {
+			return (a/b);
 		}
 
 	}
